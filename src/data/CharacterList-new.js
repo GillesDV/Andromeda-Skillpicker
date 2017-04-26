95c6ee465010b231aa55e26a6da2583d749131e2
GillesDV <gilles.devylder@hotmail.com>
2017-04-26 21:43:39 +0200

    Add new architecture for Class List. To be consumed later

---
+++ src/data/CharacterList-new.js
@@ -0,0 +1,209 @@
+let dataStore = {
+  classList: {
+    HumanMaleAdept: {
+      Name: "Human Male Adept",
+      Rarity: "Common",
+      Health: "500",
+      Shields: "250",
+      Class: "Adept"
+    },
+    HumanFemaleAdept: {
+      Name: "Human Female Adept",
+      Rarity: "Common",
+      Health: "500",
+      Shields: "250",
+      Class: "Adept"
+    },
+    AsariAdept: {
+      Name: "Asari Adept",
+      Rarity: "Uncommon",
+      Health: "475",
+      Shields: "250",
+      Class: "Adept"
+    },
+
+    HumanMaleEngineer: {
+      Name: "Human Male Engineer",
+      Rarity: "Common",
+      Health: "500",
+      Shields: "250",
+      Class: "Engineer"
+    },
+    HumanFemaleEngineer: {
+      Name: "Human Female Engineer",
+      Rarity: "Common",
+      Health: "500",
+      Shields: "250",
+      Class: "Engineer"
+    },
+    KroganEngineer: {
+      Name: "Krogan Engineer",
+      Rarity: "Uncommon",
+      Health: "600",
+      Shields: "250",
+      Class: "Engineer"
+    },
+
+    HumanMaleInfiltrator: {
+      Name: "Human Male Infiltrator",
+      Rarity: "Common",
+      Health: "500",
+      Shields: "250",
+      Class: "Infiltrator"
+    },
+    HumanFemaleInfiltrator: {
+      Name: "Human Female Infiltrator",
+      Rarity: "Common",
+      Health: "500",
+      Shields: "250",
+      Class: "Infiltrator"
+    },
+    SalarianInfiltrator: {
+      Name: "Salarian Infiltrator",
+      Rarity: "Uncommon",
+      Health: "425",
+      Shields: "270",
+      Class: "Infiltrator"
+    },
+
+    HumanMaleSoldier: {
+      Name: "Human Male Soldier",
+      Rarity: "Common",
+      Health: "500",
+      Shields: "250",
+      Class: "Soldier"
+    },
+    HumanFemaleSoldier: {
+      Name: "Human Female Soldier",
+      Rarity: "Common",
+      Health: "500",
+      Shields: "250",
+      Class: "Soldier"
+    },
+    TurianSoldier: {
+      Name: "Turian Soldier",
+      Rarity: "Uncommon",
+      Health: "500",
+      Shields: "275",
+      Class: "Soldier"
+    },
+
+    HumanMaleSentinel: {
+      Name: "Human Male Sentinel",
+      Rarity: "Common",
+      Health: "500",
+      Shields: "250",
+      Class: "Sentinel"
+    },
+    HumanFemaleSentinel: {
+      Name: "Human Female Sentinel",
+      Rarity: "Common",
+      Health: "500",
+      Shields: "250",
+      Class: "Sentinel"
+    },
+    AsariSentinel: {
+      Name: "Asari Sentinel",
+      Rarity: "Uncommon",
+      Health: "475",
+      Shields: "250",
+      Class: "Sentinel"
+    },
+
+    HumanMaleVanguard: {
+      Name: "Human Male Vanguard",
+      Rarity: "Common",
+      Health: "500",
+      Shields: "250",
+      Class: "Vanguard"
+    },
+    HumanFemaleVanguard: {
+      Name: "Human Female Vanguard",
+      Rarity: "Common",
+      Health: "500",
+      Shields: "250",
+      Class: "Vanguard"
+    },
+    KroganVanguard: {
+      Name: "Krogan Vanguard",
+      Rarity: "Uncommon",
+      Health: "600",
+      Shields: "180",
+      Class: "Vanguard"
+    },
+
+    KroganMercenary: {
+      Name: "Krogan Mercenary",
+      Rarity: "Rare",
+      Health: "700",
+      Shields: "250",
+      Class: "Other"
+    },
+    TurianHavocTrooper: {
+      Name: "Turian Havoc Trooper",
+      Rarity: "Rare",
+      Health: "500",
+      Shields: "350",
+      Class: "Other"
+    },
+    AsariHuntress: {
+      Name: "Asari Huntress",
+      Rarity: "Rare",
+      Health: "475",
+      Shields: "250",
+      Class: "Other"
+    },
+    SalarianArchitect: {
+      Name: "Salarian Architect",
+      Rarity: "Rare",
+      Health: "429",
+      Shields: "270",
+      Class: "Other"
+    },
+    AngaraInsurgent: {
+      Name: "Angara Insurgent",
+      Rarity: "Rare",
+      Health: "550",
+      Shields: "250",
+      Class: "Other"
+    },
+    AsariDuelist: {
+      Name: "Asari Duelist",
+      Rarity: "Ultra-Rare",
+      Health: "475",
+      Shields: "250",
+      Class: "Other"
+    },
+    AngaraDuelist: {
+      Name: "Angara Duelist",
+      Rarity: "Ultra-Rare",
+      Health: "500",
+      Shields: "300",
+      Class: "Other"
+    },
+    SalarianOperator: {
+      Name: "Salarian Operator",
+      Rarity: "Ultra-Rare",
+      Health: "425",
+      Shields: "270",
+      Class: "Other"
+    },
+    HumanKineticist: {
+      Name: "Human Kineticist",
+      Rarity: "Ultra-Rare",
+      Health: "450",
+      Shields: "250",
+      Class: "Other"
+    },
+    KroganGladiator: {
+      Name: "Krogan Gladiator",
+      Rarity: "Ultra-Rare",
+      Health: "700",
+      Shields: "300",
+      Class: "Other"
+    }
+
+  }
+}
+
+export {dataStore};
