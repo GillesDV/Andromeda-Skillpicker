d33bd9356e5c6278ca83478cc51186b77f3beb98
GillesDV <gilles.devylder@hotmail.com>
2017-06-26 15:32:18 +0200

    remove unused files

--- src/actions/Actions.js
+++ src/actions/Actions.js
@@ -1,18 +1,5 @@
-
 export const setClassLevel = (classNumber, skillPoints) => ({
   type: 'SET_CLASS_LEVEL',
   classNumber,
   skillPoints
 });
-
-export const setSkillPointsTotal = (skillPointsTotal) => ({
-  type: 'SET_SKILLPOINTS_TOTAL',
-  skillPointsTotal
-});
-
-/*
-export const setSkillPointsInUse = (skillPointsInUse) => ({
-  type: 'SET_SKILLPOINTS_IN_USE',
-  skillPointsInUse
-});
-*/
