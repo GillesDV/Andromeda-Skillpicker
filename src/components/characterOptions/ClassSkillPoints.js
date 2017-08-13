6b8b1f14557b43582326b86b85cc84fe3a4e22cd
GillesDV <gilles.devylder@hotmail.com>
2017-08-13 14:01:24 +0200

    correct silly copy/paste error

--- src/components/characterOptions/ClassSkillPoints.js
+++ src/components/characterOptions/ClassSkillPoints.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class ClassLevelHeader extends Component{
+export default class ClassSkillPoints extends Component{
   render(){
     return (
       <div>
